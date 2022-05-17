2ce0331875222a4e93e5f25b00ef72c16767c58b
Mateusz Ledwoń <mateusz.ledwon@iteo.com>
2022-05-17 20:59:46 +0200

    add build.yml github action

--- packages/cached/test/fixture/example.dart
+++ packages/cached/test/fixture/example.dart
@@ -9,7 +9,7 @@ class Example with _$Example {
   @Cached(syncWrite: true, ttl: 30, limit: 10)
   Future<int> call(
     String arg1, {
-    @IgnoreCache(useCacheOnError: true) bool? ignoreCache,
+    @IgnoreCache(useCacheOnError: true) bool ignoreCache = true,
   }) {
     return Future.value(5);
   }
