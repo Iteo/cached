54940de9f85c6d838b996124421c311abe525ada
Jan Lukas <jan.lukas@iteo.com>
2022-06-08 07:47:38 +0200

    created ignore annotation class

--- packages/cached_annotation/lib/cached_annotation.dart
+++ packages/cached_annotation/lib/cached_annotation.dart
@@ -3,5 +3,6 @@ library cached_annotation;
 export 'src/cached.dart';
 export 'src/clear_all_cached.dart';
 export 'src/clear_cached.dart';
+export 'src/ignore.dart';
 export 'src/ignore_cache.dart';
 export 'src/with_cache.dart';
---
+++ packages/cached_annotation/lib/src/ignore.dart
@@ -0,0 +1,8 @@
+import 'package:meta/meta_meta.dart';
+
+const ignore = Ignore();
+
+@Target({TargetKind.parameter})
+class Ignore {
+  const Ignore();
+}
