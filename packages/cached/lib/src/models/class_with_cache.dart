9f0b34c36ba6c5ec4867a00e994e54969c76d65e
Kamil Faliński <kamil.falinski@gmail.com>
2024-02-27 13:08:42 +0100

    feat: persistent_storage_annotation

--- packages/cached/lib/src/models/class_with_cache.dart
+++ packages/cached/lib/src/models/class_with_cache.dart
@@ -1,19 +1,15 @@
 import 'package:analyzer/dart/element/element.dart';
 import 'package:cached/src/config.dart';
 import 'package:cached/src/models/cache_peek_method.dart';
-import 'package:cached/src/models/cached_function.dart';
 import 'package:cached/src/models/cached_method.dart';
 import 'package:cached/src/models/clear_all_cached_method.dart';
 import 'package:cached/src/models/clear_cached_method.dart';
 import 'package:cached/src/models/constructor.dart';
 import 'package:cached/src/models/deletes_cache_method.dart';
 import 'package:cached/src/models/getters/cached_getter.dart';
-import 'package:cached/src/models/getters/lazy_persistent_cached_getter.dart';
-import 'package:cached/src/models/getters/persistent_cached_getter.dart';
-import 'package:cached/src/models/lazy_persistent_cached_method.dart';
-import 'package:cached/src/models/persistent_cached_method.dart';
 import 'package:cached/src/models/streamed_cache_method.dart';
 import 'package:cached/src/utils/asserts.dart';
+import 'package:cached/src/utils/cached_functions_generator.dart';
 import 'package:cached/src/utils/utils.dart';
 import 'package:cached_annotation/cached_annotation.dart';
 import 'package:collection/collection.dart';
@@ -56,66 +52,19 @@ class ClassWithCache {
         .map((element) => Constructor.fromElement(element, config))
         .first;
 
-    final lazyPersistentMethods = element.methods
-        .where(CachedFunction.hasCachedAnnotation<LazyPersistentCached>)
-        .map((e) => LazyPersistentCachedMethod.fromElement(e, config));
-
-    final persistentMethods = element.methods
-        .where(CachedFunction.hasCachedAnnotation<PersistentCached>)
-        .where(
-          (element) =>
-              lazyPersistentMethods.none((e) => e.name == element.name),
-        )
-        .map((e) => PersistentCachedMethod.fromElement(e, config));
-
-    final cachedMethods = element.methods
-        .where(CachedFunction.hasCachedAnnotation<Cached>)
-        .where(
-          (element) =>
-              persistentMethods.none((e) => e.name == element.name) &&
-              lazyPersistentMethods.none((e) => e.name == element.name),
-        )
-        .map((e) => CachedMethod<Cached>.fromElement(e, config));
-
-    final methods = [
-      ...cachedMethods,
-      ...persistentMethods,
-      ...lazyPersistentMethods,
-    ];
+    final methods = CachedFunctionsGenerator.generateMethods(
+      element.methods,
+      config,
+    );
 
     final methodsWithTtls = methods
         .where((method) => method.ttl != null)
         .map((method) => method.name);
 
-    final lazyPersistentGetters = element.accessors
-        .where((element) => element.isGetter)
-        .where(CachedFunction.hasCachedAnnotation<LazyPersistentCached>)
-        .map((e) => LazyPersistentCachedGetter.fromElement(e, config));
-
-    final persistentGetters = element.accessors
-        .where((element) => element.isGetter)
-        .where(CachedFunction.hasCachedAnnotation<PersistentCached>)
-        .where(
-          (element) =>
-              lazyPersistentMethods.none((e) => e.name == element.name),
-        )
-        .map((e) => PersistentCachedGetter.fromElement(e, config));
-
-    final cachedGetters = element.accessors
-        .where((element) => element.isGetter)
-        .where(
-          (element) =>
-              CachedFunction.hasCachedAnnotation<Cached>(element) &&
-              persistentMethods.none((e) => e.name == element.name) &&
-              lazyPersistentMethods.none((e) => e.name == element.name),
-        )
-        .map((e) => CachedGetter<Cached>.fromElement(e, config));
-
-    final getters = [
-      ...cachedGetters,
-      ...persistentGetters,
-      ...lazyPersistentGetters,
-    ];
+    final getters = CachedFunctionsGenerator.generateGetters(
+      element.accessors,
+      config,
+    );
 
     final gettersWithTtls = getters
         .where((getter) => getter.ttl != null)
